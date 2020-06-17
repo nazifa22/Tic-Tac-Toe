e2bb0ad0851627a87a9d2da8dc9a39411069442b
nazifa <nazifarashid1822@gmail.com>
2020-06-18 00:27:41 +0600

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
+// import React from 'react';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TicTacToe/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/Components/TicTacToe/TicTacToe.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './TicTacToe.css'
+
+class TicTacToe extends Component {
+    render() {
+        return (
+            <>
+                <h1><i>Tic Tac Toe</i></h1>
+            </>
+        )
+    }
+}
+
+export default TicTacToe;
